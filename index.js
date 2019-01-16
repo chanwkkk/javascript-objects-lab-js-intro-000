941404081a1415067455c2a3ba3c64d3dfeb1c1f
Yingqi Chen <chanwk18@hotmail.com>
2019-01-16 08:09:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var recipes=new Object();
+
 function updateObjectWithKeyAndValue(object, key, value){
   var obj2=Object.assign({},object,{[key]:value});
   return obj2;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  return object;
 }
