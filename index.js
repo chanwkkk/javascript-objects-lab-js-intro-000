00c67fac15b949e425da98caef7e0162a81f70b5
Yingqi Chen <chanwk18@hotmail.com>
2019-01-16 08:11:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key]=value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key){
+  var obj2=Object.assign({},object);
+  delete obj2[key];
+  return obj2;
+}
